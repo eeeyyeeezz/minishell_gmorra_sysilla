9ff34f76578c332ec0de1a2363d4f880ee7bf213
Stoops Ysilla <sysilla@si-f5.kzn.21-school.ru>
2021-07-13 18:19:45 +0300

    функции для основных редиректов

--- src/for_redirect.c
+++ src/for_redirect.c
@@ -9,4 +9,55 @@ int red(int fd_out, int fd_in, t_env *env)
 	fd_out = open("НАЗВАНИЕ ФАЙЛА", O_WRONLY | O_WRONLY | O_TRUNC, 0644);
 	// <
 	fd_in = open("НАЗВАНИЕ ФАЙЛА", O_RDONLY, 0644);
+	return (0);
+}
+
+int		double_right(char *name)
+{
+	int	fd;
+
+	if (!name)
+		return (0);
+	fd = open(name, O_WRONLY | O_CREAT | O_APPEND, 00644);
+	if (fd <= 0)
+	{
+		printf("%s\n", strerror(errno));
+		return (-1);
+	}
+	return (fd);
+}
+
+int		single_right(char *name)
+{
+	int	fd;
+
+	if (!name)
+		return (0);
+	fd = open(name, O_WRONLY | O_CREAT | O_TRUNC, 00644);
+	if (fd <= 0)
+	{
+		printf("%s\n", strerror(errno));
+		return (-1);
+	}
+	return (fd);
+}
+
+int		single_left(char *name)
+{
+	int	fd;
+
+	if (!name)
+		return (0);
+	fd = open(name, O_RDONLY, 00644);
+	if (fd <= 0)
+	{
+		printf("%s\n", strerror(errno));
+		return (-1);
+	}
+	return (fd);
+}
+
+int		double_left(char *name)
+{
+	/*ЧТОПРОИСХОДИТ?!!!??!*/
 }
